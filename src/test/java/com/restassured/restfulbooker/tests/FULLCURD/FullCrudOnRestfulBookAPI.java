280bd9f4ea590e4d2471dd07a081f38be5525d84
Madhu Bojja <madhubojja90@gmail.com>
2025-07-27 12:35:56 +0530

    Crud operations 2nd commit

---
+++ src/test/java/com/restassured/restfulbooker/tests/FULLCURD/FullCrudOnRestfulBookAPI.java
@@ -0,0 +1,96 @@
+package com.restassured.restfulbooker.tests.FULLCURD;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.hamcrest.Matchers;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class FullCrudOnRestfulBookAPI {
+    RequestSpecification requestSpecification= RestAssured.given();
+    Response response;
+    ValidatableResponse validatableResponse;
+    String token;
+    Integer bookingId;
+
+
+    String BASE_URI="https://restful-booker.herokuapp.com";
+
+    @BeforeTest
+    public void getToken()
+    {
+        String BASE_PATH="/auth";
+        String payload="{\n" +
+                "    \"username\" : \"admin\",\n" +
+                "    \"password\" : \"password123\"\n" +
+                "}";
+        requestSpecification.baseUri(BASE_URI);
+        requestSpecification.basePath(BASE_PATH);
+        requestSpecification.contentType(ContentType.JSON);
+        requestSpecification.body(payload);
+        response=requestSpecification.when().log().all().post();
+        validatableResponse=response.then().log().all().statusCode(200);
+        token=response.then().log().all().extract().path("token");
+        System.out.println("Token Generated " +token);
+        Assert.assertNotNull(token);
+    }
+
+@BeforeTest
+public void getBookingId()
+{
+    String BASE_PATH="/booking" ;
+    String PAYLOAD="{\n" +
+            "    \"firstname\" : \"Jim\",\n" +
+            "    \"lastname\" : \"Brown\",\n" +
+            "    \"totalprice\" : 111,\n" +
+            "    \"depositpaid\" : true,\n" +
+            "    \"bookingdates\" : {\n" +
+            "        \"checkin\" : \"2018-01-01\",\n" +
+            "        \"checkout\" : \"2019-01-01\"\n" +
+            "    },\n" +
+            "    \"additionalneeds\" : \"Breakfast\"\n" +
+            "}";
+    requestSpecification.baseUri(BASE_URI);
+    requestSpecification.basePath(BASE_PATH);
+    requestSpecification.contentType(ContentType.JSON);
+    requestSpecification.body(PAYLOAD);
+    Response response=requestSpecification.when().post();
+    ValidatableResponse validatableResponse= response.then();
+    validatableResponse.log().all().statusCode(200);
+    bookingId=response.then().extract().path("bookingid");
+    Assert.assertNotNull(bookingId);
+}
+    @Test
+    public void fullUpdateBooking_ShouldReturn200_WhenIsValid()
+    {
+
+        String BASE_PATH_UPDATED="/booking"+"/"+bookingId;
+        String PAYLOAD="{\n" +
+                "    \"firstname\" : \"Madhu\",\n" +
+                "    \"lastname\" : \"Bojja\",\n" +
+                "    \"totalprice\" : 111,\n" +
+                "    \"depositpaid\" : true,\n" +
+                "    \"bookingdates\" : {\n" +
+                "        \"checkin\" : \"2018-01-01\",\n" +
+                "        \"checkout\" : \"2019-01-01\"\n" +
+                "    },\n" +
+                "    \"additionalneeds\" : \"Breakfast\"\n" +
+                "}";
+
+        requestSpecification.baseUri(BASE_URI);
+        requestSpecification.basePath(BASE_PATH_UPDATED);
+        requestSpecification.contentType(ContentType.JSON);
+        requestSpecification.cookie("token",token);
+        requestSpecification.body(PAYLOAD);
+        response=requestSpecification.when().log().all().put();
+        validatableResponse= response.then();
+        validatableResponse.log().all().statusCode(200);
+        validatableResponse.body("firstname", Matchers.equalTo("Madhu"));
+        validatableResponse.body("lastname", Matchers.equalTo("Bojja"));
+    }
+
+}
